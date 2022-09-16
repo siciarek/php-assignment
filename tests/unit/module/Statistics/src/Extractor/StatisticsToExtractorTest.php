4e45d8686328f213381c4f67068c308cafb3a3da
Jacek Siciarek <jacek.siciarek@nokaut.pl>
2022-09-16 17:58:07 +0200

    Add test

--- tests/unit/module/Statistics/src/Extractor/StatisticsToExtractorTest.php
+++ tests/unit/module/Statistics/src/Extractor/StatisticsToExtractorTest.php
@@ -1,10 +1,12 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace Tests\unit;
 
 use PHPUnit\Framework\TestCase;
+use Statistics\Extractor\StatisticsToExtractor;
+use Statistics\Dto\StatisticsTo;
 
 /**
  * Class ATestTest
@@ -13,11 +15,25 @@ use PHPUnit\Framework\TestCase;
  */
 class StatisticsToExtractorTest extends TestCase
 {
+    const REQUIRED_KEYS = ["name", "label", "value", "units", "splitPeriod", "children"];
+
+    public function setUp(): void
+    {
+        $this->srv = new StatisticsToExtractor();
+    }
+
     /**
      * @test
      */
-    public function testExtract(): void
+    public function extractBasic(): void
     {
-        $this->assertTrue(true);
+        $stats = new StatisticsTo();
+        $labels = [];
+        $given = $this->srv->extract($stats, $labels);
+        $this->assertNotNull($given);
+
+        foreach ($this::REQUIRED_KEYS as $key) {
+            $this->assertArrayHasKey($key, $given);
+        }
     }
 }
